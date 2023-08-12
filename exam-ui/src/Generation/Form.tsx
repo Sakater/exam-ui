e3574618986bccb852748033f2f03618b6a30244
Yusuf <yusufsolmaz97@gmail.com>
2023-08-12 13:02:22 +0200

    trash 2.0

--- exam-ui/src/Generation/Form.tsx
+++ exam-ui/src/Generation/Form.tsx
@@ -2,8 +2,8 @@ import React, {ChangeEvent, useState} from "react";
 import {FileDetailsProps, Task, File} from "../interfaces/Task";
 import PDFFile from "./PDFFile";
 
-
-export default function Form(files:FileDetailsProps) {
+//TODO: überlegen wie das System mit der Zurodnung klappen könnte (UUID zu extrem?)
+export default function Form(files: FileDetailsProps) {
     const [file, setFile] = useState(files.files);
 
     // Definiere ein Mapping von Feldnamen zu ihren Typen
@@ -39,8 +39,79 @@ export default function Form(files:FileDetailsProps) {
         console.log(file);
     }
 
-    return (
+    function addQuestion(index: string) {
+        const number = parseInt(index)
+        const task: Task = {question: 'question', optionA: '', optionB: '', optionC: '', optionD: ''}
 
+        setFile(prevData => {
+            const newTasks: Task[] = [...prevData.tasks];
+            newTasks.splice(number, 0, task);
+            return {
+                ...prevData, tasks: newTasks
+            }
+        })
+
+        return (
+            <div className="row pt-4">
+                <div className="col-md-12">
+                    <label htmlFor="inputPassword5"
+                           className="col-md-form-label">Frage {number + 1}</label>
+                </div>
+                <div className="col-md-12">
+                    <input type="text" id={index}
+                           className="form-control"
+                           value={file.tasks[number].question}
+                           onChange={(e) => handleTaskChange(e, number)}
+                           aria-describedby="passwordHelpBlock"/>
+                </div>
+            </div>
+        );
+    }
+
+    function deleteQuestion(index: string) {
+
+    }
+
+    //TODO: index korrigieren (haut so nicht hin)
+    function addOption(index: string[][]) {
+        return (<div id={index[0][0]}>
+                <div className="py-1 row row-cols-4 px-0 mx-sm-1">
+                    <div className="col-md-1">A)</div>
+                    <div className="col-md-5"><input type="text" id="inputPassword5"
+                                                     className="form-control"
+                                                     name="optionA"
+                                                     onChange={(e) => handleTaskChange(e, 0)}
+                                                     aria-describedby="passwordHelpBlock"/>
+                    </div>
+                    <div className="col-md-1">B)</div>
+                    <div className="col-md-5"><input type="text" id="inputPassword5"
+                                                     className="form-control"
+                                                     name="optionB"
+                                                     onChange={(e) => handleTaskChange(e, 0)}
+                                                     aria-describedby="passwordHelpBlock"/>
+                    </div>
+                </div>
+                <div className="py-1 row row-cols-4 px-0 mx-sm-1">
+                    <div className="col-md-1 ">C)</div>
+                    <div className="col-md-5"><input type="text" id="inputPassword5"
+                                                     className="form-control"
+                                                     name="optionC"
+                                                     onChange={(e) => handleTaskChange(e, 0)}
+                                                     aria-describedby="passwordHelpBlock"/>
+                    </div>
+                    <div className="col-md-1">D)</div>
+                    <div className="col-md-5"><input type="text" id="inputPassword5"
+                                                     className="form-control"
+                                                     name="optionD"
+                                                     onChange={(e) => handleTaskChange(e, 0)}
+                                                     aria-describedby="passwordHelpBlock"/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
         <div className="row row-col-2">
             <div className="col-md-8 pt-5">
 
@@ -55,25 +126,19 @@ export default function Form(files:FileDetailsProps) {
                                className="form-control"
                                value={file.title}
                                name="title"
-                               onChange={handleChange}
+                               onChange={(e) => handleChange(e)}
                                aria-describedby="passwordHelpBlock"/>
                     </div>
                 </div>
 
-                {/*Frage 1*/}
-                <div className="row pt-4">
-                    <div className="col-md-12">
-                        <label htmlFor="inputPassword5"
-                               className="col-md-form-label">Frage 1</label>
-                    </div>
-                    <div className="col-md-12"><input type="text" id="inputPassword5"
-                                                      className="form-control"
-                                                      value={file.tasks[0].question}
-                                                      onChange={handleChange}
-                                                      aria-describedby="passwordHelpBlock"/></div>
-                </div>
+                {/*Frage 1*/
+                }
+                <button onClick={() => addQuestion("7098")}>Füge eine Frage hinzu</button>
+                <button onClick={() => addOption([[("7098"), ("afsdk")], [("djhdsjh")]])}>Füge weitere optionen hinzu
+                </button>
 
-                {/*Frage 2*/}
+                {/*Frage 2*/
+                }
                 <div className="row pt-4">
                     <div className="col-md-12">
                         <label htmlFor="inputPassword5"
@@ -85,38 +150,7 @@ export default function Form(files:FileDetailsProps) {
                                                       onChange={(e) => handleTaskChange(e, 0)}
                                                       aria-describedby="passwordHelpBlock"/>
                     </div>
-                    <div className="py-1 row row-cols-4 px-0 mx-sm-1">
-                        <div className="col-md-1">A)</div>
-                        <div className="col-md-5"><input type="text" id="inputPassword5"
-                                                         className="form-control"
-                                                         name="optionA"
-                                                         onChange={(e) => handleTaskChange(e, 0)}
-                                                         aria-describedby="passwordHelpBlock"/>
-                        </div>
-                        <div className="col-md-1">B)</div>
-                        <div className="col-md-5"><input type="text" id="inputPassword5"
-                                                         className="form-control"
-                                                         name="optionB"
-                                                         onChange={(e) => handleTaskChange(e, 0)}
-                                                         aria-describedby="passwordHelpBlock"/>
-                        </div>
-                    </div>
-                    <div className="py-1 row row-cols-4 px-0 mx-sm-1">
-                        <div className="col-md-1 ">C)</div>
-                        <div className="col-md-5"><input type="text" id="inputPassword5"
-                                                         className="form-control"
-                                                         name="optionC"
-                                                         onChange={(e) => handleTaskChange(e, 0)}
-                                                         aria-describedby="passwordHelpBlock"/>
-                        </div>
-                        <div className="col-md-1">D)</div>
-                        <div className="col-md-5"><input type="text" id="inputPassword5"
-                                                         className="form-control"
-                                                         name="optionD"
-                                                         onChange={(e) => handleTaskChange(e, 0)}
-                                                         aria-describedby="passwordHelpBlock"/>
-                        </div>
-                    </div>
+
 
                 </div>
             </div>
@@ -129,5 +163,6 @@ export default function Form(files:FileDetailsProps) {
         </div>
 
 
-    );
+    )
+        ;
 }
--- exam-ui/src/Generation/PDFFile.tsx
+++ exam-ui/src/Generation/PDFFile.tsx
@@ -1,16 +1,20 @@
 import React, {useState} from "react";
 import {PDFViewer, Document, Text, Page} from "@react-pdf/renderer";
-import {FileDetailsProps} from "../interfaces/Task";
+import {FileDetailsProps, File} from "../interfaces/Task";
 
 function PDFFile(files:FileDetailsProps) {
-    const [file, setFile] = useState(files.files);
+    const [file, setFile] = useState<File>(files.files);
+    console.log(file)
     return (
         <div>
             <PDFViewer height={500} showToolbar={true}>
                 <Document pageLayout={"singlePage"}>
                     <Page size={"A4"}>
                         <Text>
-                           <h1>Hallo</h1> Hallo
+                           <h1>{file.title}</h1>
+                            <br/>
+                            <br/>
+                            1. Frage: {file.tasks.at(0)?.question&& <h2>{file.tasks[0].question}</h2>}
                         </Text>
                     </Page>
                 </Document>
