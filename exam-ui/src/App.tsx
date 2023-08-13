e10b8ef934d8bfbfe61945fad6864c682f012512
Yusuf <yusufsolmaz97@gmail.com>
2023-08-14 01:43:36 +0200

    Erstellen von Fragen Plus optional mc

--- exam-ui/src/App.tsx
+++ exam-ui/src/App.tsx
@@ -2,15 +2,27 @@ import React, {useState} from 'react';
 import './App.css';
 import Form from "./Generation/Form";
 import PDFFile from "./Generation/PDFFile";
-import {FileDetailsProps} from "./interfaces/Task";
+import {FileDetailsProps, File} from "./interfaces/Task";
+import { v4 as uuidv4 } from "uuid";
+
 
 function App() {
+        const initialFileState: File = {
+            tasks: [
+                {
+                    question: '',
+                    options: {},
+                    id: uuidv4(),
+                }
+            ],
+            title: ''
+        };
     return (
 
         <div className="App">
             <body>
             <div className="container text-center">
-                <Form/>
+                <Form fileDetailsProps={{ files: initialFileState }} />
             </div>
             </body>
         </div>
--- exam-ui/src/Generation/Form.tsx
+++ exam-ui/src/Generation/Form.tsx
@@ -1,141 +1,10 @@
-/*
 import React, { ChangeEvent, useState } from "react";
-import { Task, File } from "../interfaces/Task";
-import PDFFile from "./PDFFile";
-
-export default function Form() {
-    const initialFileState: File = {
-        tasks: [
-            {
-                question: '',
-                optionA: '',
-                optionB: '',
-                optionC: '',
-                optionD: '',
-                id: null as any
-            }
-        ],
-        title: ''
-    };
-
-    const [file, setFile] = useState<File>(initialFileState);
-
-    function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
-        const { name, value } = event.target;
-        setFile(prevData => ({
-            ...prevData,
-            [name]: value
-        }));
-    }
-
-    function handleTaskChange(event: ChangeEvent<HTMLInputElement>, index: number, name: keyof Task): void {
-        const { value } = event.target;
-        const typedValue: string = value;
-        setFile(prevData => {
-            const newTasks: Task[] = [...prevData.tasks];
-            newTasks[index]["question"] = typedValue
-            return {
-                ...prevData,
-                tasks: newTasks
-            };
-        });
-    }
-
-    function addQuestion() {
-        const newTask: Task = {
-            question: '',
-            optionA: '',
-            optionB: '',
-            optionC: '',
-            optionD: '',
-            id: Date.now() // Use a more appropriate method to generate IDs
-        };
-
-        setFile(prevData => ({
-            ...prevData,
-            tasks: [...prevData.tasks, newTask]
-        }));
-    }
-
-    return (
-        <div className="row">
-            <div className="col-md-8 pt-5">
-                <div className="row">
-                    <div className="col-md-4">
-                        <label htmlFor="inputPassword5" className="col-md-form-label">Überschrift</label>
-                    </div>
-                    <div className="col-md-8">
-                        <input
-                            type="text"
-                            id="inputPassword5"
-                            className="form-control"
-                            value={file.title}
-                            name="title"
-                            onChange={handleFileChange}
-                            aria-describedby="passwordHelpBlock"
-                        />
-                    </div>
-                </div>
-
-                <button onClick={addQuestion}>Füge eine Frage hinzu</button>
-
-                {file.tasks.map((task, index) => (
-                    <div key={task.id}>
-                        {/!* Render question input *!/}
-                        <input
-                            type="text"
-                            className="form-control"
-                            value={task.question}
-                            name="question"
-                            onChange={(e) => handleTaskChange(e, index, 'question')}
-                        />
-
-                        {/!* Render options input *!/}
-                        <input
-                            type="text"
-                            className="form-control"
-                            value={task.optionA}
-                            name="optionA"
-                            onChange={(e) => handleTaskChange(e, index, 'optionA')}
-                        />
-                        {/!* Render other options similarly *!/}
-                    </div>
-                ))}
-
-                {/!* ... other code ... *!/}
-
-            </div>
-
-            <div className="col-md-4">
-                <PDFFile files={file} />
-            </div>
-        </div>
-    );
-}
-*/
-import React, { ChangeEvent, useState } from "react";
-import { Task, File } from "../interfaces/Task";
+import { Task, File, FileDetailsProps } from "../interfaces/Task";
 import PDFFile from "./PDFFile";
 import { v4 as uuidv4 } from "uuid";
 
-const options = ['A', 'B'];
-
-export default function Form() {
-    const initialFileState: File = {
-        tasks: [
-            {
-                question: '',
-                options: options.reduce((obj, option) => {
-                    obj[option] = '';
-                    return obj;
-                }, {} as Record<string, string>),
-                id: null as any,
-            },
-        ],
-        title: '',
-    };
-
-    const [file, setFile] = useState<File>(initialFileState);
+export default function Form({ fileDetailsProps }: { fileDetailsProps: FileDetailsProps }) {
+    const [file, setFile] = useState<File>(fileDetailsProps.files);
 
     function handleFileChange(event: ChangeEvent<HTMLInputElement>) {
         const { name, value } = event.target;
@@ -145,11 +14,11 @@ export default function Form() {
         }));
     }
 
-    function handleTaskChange(event: ChangeEvent<HTMLInputElement>, index: number, option: keyof Task['options']): void {
+    function handleTaskChange(event: ChangeEvent<HTMLInputElement>, index: number, name: keyof Task['options']): void {
         const { value } = event.target;
         setFile((prevData) => {
             const newTasks: Task[] = [...prevData.tasks];
-            newTasks[index].options[option] = value;
+            newTasks[index].options[name] = value;
             return {
                 ...prevData,
                 tasks: newTasks,
@@ -160,17 +29,36 @@ export default function Form() {
     function addQuestion() {
         const newTask: Task = {
             question: '',
-            options: options.reduce((obj, option) => {
-                obj[option] = '';
-                return obj;
-            }, {} as Record<string, string>),
-            id: uuidv4(), // Use a more appropriate method to generate IDs
+            options: {},
+            id: uuidv4(),
         };
 
         setFile((prevData) => ({
             ...prevData,
             tasks: [...prevData.tasks, newTask],
         }));
+    }
+
+    function addOption(index: number) {
+        if (index >= 0 && index < file.tasks.length) {
+            const task = file.tasks[index];
+            const newOptionKey = String.fromCharCode(65 + Object.keys(task.options).length); // Start from 'A'
+
+            setFile((prevData) => {
+                const newTasks: Task[] = [...prevData.tasks];
+                newTasks[index] = {
+                    ...newTasks[index],
+                    options: {
+                        ...newTasks[index].options,
+                        [newOptionKey]: '',
+                    },
+                };
+                return {
+                    ...prevData,
+                    tasks: newTasks,
+                };
+            });
+        }
     }
 
     return (
@@ -204,27 +92,25 @@ export default function Form() {
                             type="text"
                             className="form-control"
                             value={task.question}
-                            name="question"
+                            name={`question_${index}`}
                             onChange={(e) => handleTaskChange(e, index, 'question')}
                         />
 
-                        {/* Render options input */}
-                        {options.map((option) => (
+                        {/* Render options inputs */}
+                        {Object.keys(task.options).map((option) => (
                             <input
                                 key={option}
                                 type="text"
                                 className="form-control"
                                 value={task.options[option]}
-                                name={`option${option}`}
+                                name={`option_${index}_${option}`}
                                 onChange={(e) => handleTaskChange(e, index, option as keyof Task['options'])}
                             />
                         ))}
+                        <button onClick={() => addOption(index)}>Füge Option hinzu</button>
                     </div>
                 ))}
-
-                {/* ... other code ... */}
             </div>
-
             <div className="col-md-4">
                 <PDFFile files={file} />
             </div>
